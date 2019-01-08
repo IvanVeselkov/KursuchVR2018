c1bba30de703bf6872b9b6226293db91597c9b09
Unknown <nerorus97@gmail.com>
2019-01-08 19:40:06 +0400

    Камера
    ол

---
+++ FisrtVive/Assets/scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Player : MonoBehaviour {
+    public Rigidbody Mybode;
+    public float Speed;
+    private Vector3 Movement;
+    static Animator anim;
+    float Forward;
+	// Use this for initialization
+	void Start () 
+    {
+        Mybode = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //if (Forward != 0)
+        //{
+        //    anim.SetBool("isRunning", true);
+        //}
+        //else
+        //{
+        //    anim.SetBool("isRunning", false);
+        //}
+	}
+
+    void FixedUpdate()
+    {
+        float Rigth = Input.GetAxisRaw("Horizontal");
+        Forward = Input.GetAxisRaw("Vertical");
+
+        Movement.Set(Forward, 0f, Rigth);
+
+        Mybode.AddForce(transform.forward * Forward * Speed, ForceMode.Impulse);
+        Mybode.AddForce(transform.right*Rigth*Speed, ForceMode.Impulse);
+    }
+}
